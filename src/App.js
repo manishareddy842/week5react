9ee662d75270f1ada590215010b8859ea18aab94
manishareddy842 <109997654+manishareddy842@users.noreply.github.com>
2023-02-21 12:40:04 -0600

    asdfghj

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import weather from "./weather";
 export default function App(){
   return(
     <div>
+    <div className="weather">
+    <div class="conatiner">
       <form>
+        <div class="row">
+        <div class="col-8">
         <input type="text" placeholder="Enter a city.."></input>
-        <span>
+        </div>
+        <div class="col-4">
           <button type="button" class="btn btn-primary">Search</button>
-        </span>
+       </div>
+       </div>
       </form>
-      <div class ="conatiner">
-    
+    </div>
     </div>
     </div>
   )
