59cd51a8a2cf21e6e8f3bb8fb35e8c0341af726a
manishareddy842 <109997654+manishareddy842@users.noreply.github.com>
2023-04-20 16:34:56 -0500

    a

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Weather from "./Weather.js";
 export default function WeatherForecast(props){
+let  apiKey ="c809b1aac8890b5b8d6ecb54769e0f7f";
+let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 return(
     <div>
       <div className="row">
