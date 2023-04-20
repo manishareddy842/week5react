61a7c4734e57b830c9431c2799a7d75ee2f47176
manishareddy842 <109997654+manishareddy842@users.noreply.github.com>
2023-04-20 12:48:52 -0500

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -6,7 +6,9 @@ return(
       <div className="row">
         <div className="col-1">
             <div>mon</div>
-            <div>{props.thing.icon}</div>
+            <div>
+                <img src={`http://openweathermap.org/img/wn/${props.thing.icon}.png`}>{props.thing.icon}</img>
+                </div>
             <div><span>20</span>
             <span>15</span>
             </div>
