0431f5e90991309efef5b7001173da0b5df53816
manishareddy842 <109997654+manishareddy842@users.noreply.github.com>
2023-04-20 12:41:16 -0500

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -7,7 +7,8 @@ return(
         <div className="col-1">
             <div>mon</div>
             <div>{props.thing.icon}</div>
-            <div><span>20</span><span>15</span>
+            <div><span>20</span>
+            <span>15</span>
             </div>
         </div>
         </div>  
